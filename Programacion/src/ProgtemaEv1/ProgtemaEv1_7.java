b28722ee362860fbbcd00347c4ab725770325241
Hugo <alvarezherrera25@iespabloserrano.org>
2025-11-17 09:17:19 +0100

    ProgT1

--- Programacion/src/ProgtemaEv1/ProgtemaEv1_7.java
+++ Programacion/src/ProgtemaEv1/ProgtemaEv1_7.java
@@ -3,8 +3,13 @@ package ProgtemaEv1;
 import java.util.Scanner;
 
 public class ProgtemaEv1_7 {
-    public static void main(String[] args){
+    public static void main(String[ ] args){
         Scanner sc = new Scanner(System.in);
-        q
+        int horas;
+        int salario;
+        System.out.print("Introduce las horas trabajadas en la semana: ");
+        horas = sc.nextInt();
+        salario = horas * 12;
+        System.out.println("El salario semanal es de: " + salario + " euros.");
     }
 }
