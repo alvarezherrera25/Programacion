9dd1fb7ee478c51d6f23c1d5fa5dd6b2f7e2dc9e
Hugo <alvarezherrera25@iespabloserrano.org>
2025-11-19 11:10:04 +0100

    cambios

---
+++ Programacion/src/ProgtemaEv1/ProgtemaEv1_12.java
@@ -0,0 +1,15 @@
+package ProgtemaEv1;
+
+import java.util.Scanner;
+
+public class ProgtemaEv1_12 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String J1;
+        String J2;
+
+        System.out.print("Primer jugador (piedra/papel/tijera):")
+        J1 = = sc.nextLine();
+        
+    }
+}
