ae2ee437db64f4f310ba9f3feccaa1dd3070fb8d
Hugo <alvarezherrera25@iespabloserrano.org>
2025-11-21 12:15:02 +0100

    jueves 21

--- Programacion/src/ProgtemaEv1/ProgtemaEv1_12.java
+++ Programacion/src/ProgtemaEv1/ProgtemaEv1_12.java
@@ -8,8 +8,47 @@ public class ProgtemaEv1_12 {
         String J1;
         String J2;
 
-        System.out.print("Primer jugador (piedra/papel/tijera):")
-        J1 = = sc.nextLine();
+        System.out.print("Primer jugador (piedra/papel/tijera):");
+        J1 = sc.nextLine();
         
+        if (J1.equalsIgnoreCase("piedra")) {
+            System.out.print("Segundo jugador (piedra/papel/tijera):");
+            J2 = sc.nextLine();
+            if (J2.equalsIgnoreCase("piedra")) {
+                System.out.print("Empate");
+            } else if (J2.equalsIgnoreCase("tijera")) {
+                System.out.print("Gana jugador 1");
+            } else if (J2.equalsIgnoreCase("papel")) {
+                System.out.print("Gana jugador 2");
+            } else {
+                System.out.print("Opcion no reconocida");
+            }
+        } else if (J1.equalsIgnoreCase("papel")) {
+            System.out.print("Segundo jugador (piedra/papel/tijera):");
+            J2 = sc.nextLine();
+            if (J2.equalsIgnoreCase("papel")) {
+                System.out.print("Empate");
+            } else if (J2.equalsIgnoreCase("piedra")) {
+                System.out.print("Gana jugador 1");
+            } else if (J2.equalsIgnoreCase("tijera")) {
+                System.out.print("Gana jugador 2");
+            } else {
+                System.out.print("Opcion no reconocida");
+            }
+        } else if (J1.equalsIgnoreCase("tijera")) {
+            System.out.print("Segundo jugador (piedra/papel/tijera):");
+            J2 = sc.nextLine();
+            if (J2.equalsIgnoreCase("tijera")) {
+                System.out.print("Empate");
+            } else if (J2.equalsIgnoreCase("papel")) {
+                System.out.print("Gana jugador 1");
+            } else if (J2.equalsIgnoreCase("piedra")) {
+                System.out.print("Gana jugador 2");
+            } else {
+                System.out.print("Opcion no reconocida");
+            }
+        } else {
+            System.out.print("Opcion no reconocida");
+        }
     }
 }
