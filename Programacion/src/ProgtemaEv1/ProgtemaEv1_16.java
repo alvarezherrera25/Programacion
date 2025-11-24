0b4dc6046a83a85d7eceaf5f85a563d32cae7f95
Hugo <alvarezherrera25@iespabloserrano.org>
2025-11-24 10:47:44 +0100

    s

--- Programacion/src/ProgtemaEv1/ProgtemaEv1_16.java
+++ Programacion/src/ProgtemaEv1/ProgtemaEv1_16.java
@@ -11,28 +11,5 @@ public class ProgtemaEv1_16 {
         System.out.print("Introduce el NÚMERO 2 (misma longitud): ");
         long num2 = sc.nextLong();
 
-        long mezclaInversa = 0; 
-
-        while (num1 > 0) {
-            
-            long d1 = num1 % 10;
-            num1 /= 10; 
-            mezclaInversa = mezclaInversa * 10 + d1;
-
-
-            long d2 = num2 % 10;
-            num2 /= 10; 
-            mezclaInversa = mezclaInversa * 10 + d2;
-        }
-
-        long resultadoMixto = 0;
-        
-        while (mezclaInversa != 0) {
-            long digito = mezclaInversa % 10;
-            resultadoMixto = resultadoMixto * 10 + digito;
-            mezclaInversa /= 10;
-        }
-
-        System.out.println("El número mezclado final es: " + resultadoMixto);
     }
 }
