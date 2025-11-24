c80171643cf22ac4270a27934b35fbbe4de2371d
Hugo <alvarezherrera25@iespabloserrano.org>
2025-11-24 11:03:20 +0100

    s

---
+++ Programacion/src/ProgtemaEv1/ProgtemaEv1_17.java
@@ -0,0 +1,10 @@
+package ProgtemaEv1;
+
+import java.util.Scanner;
+
+public class ProgtemaEv1_17 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+    }
+}
