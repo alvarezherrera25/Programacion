fef9bd775ae91590653100c5eefef5e184660e87
Hugo <alvarezherrera25@iespabloserrano.org>
2026-02-06 13:33:19 +0100

    subir

--- Prog_2º_Trimestre/Progtema5/Progtema5_6_clase.java
+++ Prog_2º_Trimestre/Progtema5/Progtema5_6_clase.java
@@ -1,15 +1,9 @@
 package Progtema5;
 
-public class Hora {
+class Hora {
     private int hora;
     private int minutos;
     private int segundos;
-
-    public Hora() {
-        this.hora = 0;
-        this.minutos = 0;
-        this.segundos = 0;
-    }
 
     public Hora(int hora, int minutos, int segundos) {
         this.hora = hora;
@@ -23,38 +17,38 @@ public class Hora {
         this.minutos = this.minutos + m;
         this.hora = this.hora + h;
         // 1.1 Suma segundos
-        if (this.segundos >= 60) {
+        while (this.segundos >= 60) {
             this.segundos = this.segundos - 60;
             this.minutos = this.minutos + 1;
         }
         // 1.2 Suma minutos
-        if (this.minutos >= 60) {
+        while (this.minutos >= 60) {
             this.minutos = this.minutos - 60;
             this.hora = this.hora + 1;
         }
         // 1.3 Suma horas
-        if (this.hora >= 24) {
+        while (this.hora >= 24) {
             this.hora = this.hora - 24;
         }
     }
 
     // 2. Resta
     public void restar(int h, int m, int s) {
-        // 1.1 Resta segundos
+        // 2.1 Resta segundos
         this.segundos = this.segundos - s;
-        if (this.segundos < 0) {
+        while (this.segundos < 0) {
             this.segundos = this.segundos + 60;
             this.minutos = this.minutos - 1;
         }
-        // 1.1 Resta minutos
+        // 2.2 Resta minutos
         this.minutos = this.minutos - m;
-        if (this.minutos < 0) {
+        while (this.minutos < 0) {
             this.minutos = this.minutos + 60;
             this.hora = this.hora - 1;
         }
-        // 1.1 Resta horas
+        // 2.3 Resta horas
         this.hora = this.hora - h;
-        if (this.hora < 0) {
+        while (this.hora < 0) {
             this.hora = this.hora + 24;
         }
     }
--- Prog_2º_Trimestre/Progtema5/Progtema5_6_objeto.java
+++ Prog_2º_Trimestre/Progtema5/Progtema5_6_objeto.java
@@ -7,10 +7,10 @@ public class Progtema5_6_objeto {
         System.out.println("Hora inicial: " + miReloj.imprimir());
 
         // 1. Sumar tiempo directamente pasando los números
-        miReloj.sumar(0, 10, 20); 
+        miReloj.sumar(0, 120, 10); 
         System.out.println("Después de sumar: " + miReloj.imprimir());
-        // 1. Restar tiempo directamente
-        miReloj.restar(0, 2, 0);
+        // 2. Restar tiempo directamente
+        miReloj.restar(1, 75, 10);
         System.out.println("Después de restar 2 min: " + miReloj.imprimir());
     }
 }
