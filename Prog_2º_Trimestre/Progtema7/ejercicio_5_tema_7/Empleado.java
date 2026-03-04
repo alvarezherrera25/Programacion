53abf88815e6623fb586ac6736aa967e23abd313
Hugo <alvarezherrera25@iespabloserrano.org>
2026-03-04 10:55:30 +0100

    si

---
+++ Prog_2º_Trimestre/Progtema7/ejercicio_5_tema_7/Empleado.java
@@ -0,0 +1,31 @@
+package Progtema7.ejercicio_5_tema_7;
+
+abstract class Personal {
+    
+    // 6. DECLARAR ATRIBUTO ABSTRACTO
+    // ERROR: En Java NO existen los atributos abstractos. 
+    // Los atributos siempre tienen que tener un tipo y existir.
+    // abstract String nombre; // <-- Esto daría error de compilación.
+    
+    protected String nombre;
+
+    public Personal(String nombre) {
+        this.nombre = nombre;
+    }
+
+    // 1. MÉTODO NO ABSTRACTO (Tiene cuerpo)
+    public void saludar() {
+        System.out.println("Hola, soy " + nombre);
+    }
+
+    // 2. MÉTODO ABSTRACTO (Sin cuerpo, termina en ';')
+    public abstract void trabajar();
+
+    // 3. PONER CUERPO A UN MÉTODO ABSTRACTO
+    // ERROR: Un método abstracto, por definición, NO puede tener cuerpo {}.
+    /*
+    public abstract void descansar() { 
+        System.out.println("Descansando..."); 
+    } 
+    */
+}
