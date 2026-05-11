7f2a613b367dfff17a6c56c067de516c29bc7d68
alvarezherrera25 <alvarezherrera25@iespabloserrano.com>
2026-05-11 11:07:47 +0200

    a

--- Ejercicios_Finales/primer_evaluacion/ejercicio_2/Main.java
+++ Ejercicios_Finales/primer_evaluacion/ejercicio_2/Main.java
@@ -6,8 +6,29 @@ diga “P tiene más divisores que Q” o viceversa.
 Este programa debe llamar a un método que calcule los divisores de un número,
 lo aplique para P y para Q y luego compare y decida.
 */
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduce un numero entero:");
+        int P = sc.nextInt();
+        System.out.println("Introduce otro numero entero:");
+        int Q = sc.nextInt();
+
+        if (calcularDivisores(P) > calcularDivisores(Q)) {
+            System.out.println(P + " tiene mas divisores que " + Q);
+        } else {
+            System.out.println(Q + " tiene mas divisores que " + P);
+        }
+    }
+    public static int calcularDivisores(int num){
+        int divisores = 0;
+        for (int i = 1; i <= num; i++){
+            if (num % i == 0){
+                divisores++;
+            }
+        }
+        return divisores;
     }
 }
---
+++ Ejercicios_Finales/primer_evaluacion/ejercicio_3/Main.java
@@ -0,0 +1,23 @@
+package Ejercicios_Finales.primer_evaluacion.ejercicio_3;
+/*
+Se define el factorial de un número N como N*(N-1)*(N-2)*.......*3*2*1. Hacer un
+programa que lea un número N filtrando a que sea mayor que cero y calcule su
+factorial
+La función a construir aquí se llamará factorial. El método main solo debe leer
+un valor y devolver su factorial (versión iterativa).
+*/
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce un numero entero: ");
+        int N = sc.nextInt();
+
+        System.out.println("El factorial de " + N + " es: " + factorial(N));
+    }
+
+    public static int factorial(int N) {
+        int fact = 1;
+        for (int )
+    }
+}
