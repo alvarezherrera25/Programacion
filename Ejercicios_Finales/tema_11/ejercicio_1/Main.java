630de69743cdfa9f40b3a8d6732f852064540b53
Pc <alvarezherrera25@iespablogargallo.com>
2026-05-13 22:39:00 +0200

    yets

--- Ejercicios_Finales/tema_11/ejercicio_1/Main.java
+++ Ejercicios_Finales/tema_11/ejercicio_1/Main.java
@@ -1,4 +1,5 @@
 package Ejercicios_Finales.tema_11.ejercicio_1;
+
 /*
 Escribe un programa que guarde en un fichero con nombre primos.txt los
 números primos que hay entre 1 y 500. Buffered streams y sin try-with-resources (Busca en Wikipedia "Criba de
@@ -6,10 +7,13 @@ Eratóstenes)
 */
 import java.util.*;
 import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
         try {
-            BufferedWriter bw = new BufferedWriter(new FileWriter("E:\\HugoAH\\Programacion\\Ejercicios_Finales\\tema_11\\ejercicio_1\\primos.txt"));
+            BufferedWriter bw = new BufferedWriter(
+                    new FileWriter(
+                            "C:\\Users\\alvarezherrera\\Documents\\Programacion\\Ejercicios_Finales\\tema_11\\ejercicio_1\\primos.txt"));
             for (int i = 1; i <= 500; i++) {
                 if (esPrimo(i)) {
                     bw.write(i + "\n");
@@ -21,7 +25,16 @@ public class Main {
         }
 
         try {
-            BufferedReader bt = new BufferedReader(new FileReader)
+            BufferedReader br = new BufferedReader(
+                    new FileReader(
+                            "C:\\Users\\alvarezherrera\\Documents\\Programacion\\Ejercicios_Finales\\tema_11\\ejercicio_1\\primos.txt"));
+            String linea = br.readLine();
+
+            while ((linea = br.readLine()) != null) {
+                System.out.println(linea);
+            }
+        } catch (IOException e) {
+            System.out.println("Error al leer el fichero: " + e.getMessage());
         }
     }
 
